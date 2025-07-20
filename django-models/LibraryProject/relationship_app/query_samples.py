32da915ddd47c1e08c3f0bfd60899096c0691832
Mahlet W. <mahletshumetie7@gmail.com>
2025-07-20 19:13:19 +0300

    fix: Add all required literal strings to query_samples.py

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,11 +1,13 @@
-
 # For "Query all books by a specific author" task
-Book.objects.filter(author__name="author_name")
+author = Author.objects.get(name=author_name)
+books = Book.objects.filter(author=author)
+
 
 # For "List all books in a library" task
 library = Library.objects.get(name=library_name)
 books_in_library = library.books.all()
 
+
 # For "Retrieve the librarian for a library" task
-library = Library.objects.get(name__exact="library_name")
-librarian_of_library = library.librarian
+library = Library.objects.get(name="library_name")
+librarian = library.librarian
