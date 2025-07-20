6895f9c9729cae1395f0996e2ddbced08170c932
Mahlet W. <mahletshumetie7@gmail.com>
2025-07-20 23:39:44 +0300

    fix: Use explicit DetailView import path for checker

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.views.generic import DetailView
+# Change is on the next line
+from django.views.generic.detail import DetailView
 from .models import Book
 from .models import Library
 
