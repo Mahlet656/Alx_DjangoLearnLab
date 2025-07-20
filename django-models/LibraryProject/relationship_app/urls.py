3e310e646f7c95741cf4e9a32264d049bca77042
Mahlet W. <mahletshumetie7@gmail.com>
2025-07-21 00:02:27 +0300

    fix: Relocate auth templates to registration directory

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -2,8 +2,12 @@ from django.urls import path, include
 from .views import list_books, LibraryDetailView, RegisterView
 
 urlpatterns = [
+    # App-specific URLs
     path('books/', list_books, name='list_books'),
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
-    path('accounts/', include('django.contrib.auth.urls')),
     path('register/', RegisterView.as_view(), name='register'),
+    
+    # Auth URLs
+    # This will automatically look for templates in 'registration/'
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
