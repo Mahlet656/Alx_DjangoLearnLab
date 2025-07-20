ce8658c59243ed73bca53bdebea61529343befeb
Mahlet W. <mahletshumetie7@gmail.com>
2025-07-20 23:56:22 +0300

    feat: Implement user authentication

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,7 +1,9 @@
-from django.urls import path
-from .views import list_books, LibraryDetailView
+from django.urls import path, include
+from .views import list_books, LibraryDetailView, RegisterView
 
 urlpatterns = [
     path('books/', list_books, name='list_books'),
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('register/', RegisterView.as_view(), name='register'),
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.views.generic.detail import DetailView
-from .models import Book
-from .models import Library
+from .models import Book, Library
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.views import generic
 
 def list_books(request):
     books = Book.objects.all()
@@ -10,3 +12,8 @@ def list_books(request):
 class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
+
+class RegisterView(generic.CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'relationship_app/register.html'
