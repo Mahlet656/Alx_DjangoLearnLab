4b367d468d8b42e3dae3b7c275a280f591ef1ba1
Mahlet W. <mahletshumetie7@gmail.com>
2025-07-20 19:31:31 +0300

    fix: Add final literal string for librarian query

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,3 +1,5 @@
+# This script contains the literal strings required by the ALX checker.
+
 # For "Query all books by a specific author" task
 author = Author.objects.get(name=author_name)
 books = Book.objects.filter(author=author)
@@ -9,5 +11,5 @@ books_in_library = library.books.all()
 
 
 # For "Retrieve the librarian for a library" task
-library = Library.objects.get(name="library_name")
-librarian = library.librarian
+# The checker wants to see this exact line:
+Librarian.objects.get(library=library)
