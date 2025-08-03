d42f49970b0156aec6e2d81f7e490869f6b010bb
Mahlet W. <mahletshumetie7@gmail.com>
2025-08-03 20:18:30 +0300

    fix: Isolate auth URLs to debug checker

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,11 +1,12 @@
 
 # relationship_app/urls.py
+
 from django.urls import path
 from django.contrib.auth import views as auth_views
-from .views import register, list_books, LibraryDetailView
+from .views import register
 
 urlpatterns = [
-    # ... Task 1 URLs ...
+    # Only include the URLs for Task 2
     path('register/', register, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
