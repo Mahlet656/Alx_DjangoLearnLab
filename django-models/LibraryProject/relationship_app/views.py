82ac5fcdf2ed447ea958a9f5043f61faa6c0ede5
Mahlet W. <mahletshumetie7@gmail.com>
2025-08-03 18:25:28 +0300

    feat: Implement views, templates, and URLs for Task 1

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+# django-models/LibraryProject/relationship_app/views.py
 
-# Create your views here.
+from django.shortcuts import render
+from django.views.generic import DetailView
+from .models import Book, Library
+
+# Function-based view to display a list of all books
+def list_books(request):
+    all_books = Book.objects.all()
+    context = {'books': all_books}
+    return render(request, 'relationship_app/list_books.html', context)
+
+# Class-based view to display details for a single library
+# We use DetailView because it's designed to show a single object.
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = 'relationship_app/library_detail.html'
